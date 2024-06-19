ea0a861f68649877f55e1499d406044aef66d168
BisuBaniya <bisusiddharth1806@gmail.com>
2024-06-19 13:46:14 +0530

    19 June

--- djangoProject/bisu/models.py
+++ djangoProject/bisu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class SamosaVariety(models.Model):
+    SAMOSA_TYPE_CHOICE = [
+        ('PN','Paneer'),
+        ('AL','Aloo'),
+        ('PJ','Punjabi'),
+        ('PS','Pasta'),
+        ('CM','Chowmein'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='bisu/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=SAMOSA_TYPE_CHOICE)
+    

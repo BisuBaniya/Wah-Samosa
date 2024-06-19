c5aaa4b0509dbcb307fc60139cff208fd3246c97
BisuBaniya <bisusiddharth1806@gmail.com>
2024-06-19 19:47:57 +0530

    19 June

--- djangoProject/bisu/admin.py
+++ djangoProject/bisu/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
-
+from django.contrib import admin # type: ignore
+from .models import SamosaVariety # type: ignore
 # Register your models here.
+
+admin.site.register(SamosaVariety)
+

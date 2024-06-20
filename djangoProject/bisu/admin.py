5d45295a9b6d57fb3419b5392444a2cad7fa2084
BisuBaniya <bisusiddharth1806@gmail.com>
2024-06-20 22:15:22 +0530

    20 June Commit

--- djangoProject/bisu/admin.py
+++ djangoProject/bisu/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin # type: ignore
-from .models import SamosaVariety # type: ignore
+from .models import SamosaVariety,SamosaReview,Store,SamosaCertificate # type: ignore
+
 # Register your models here.
 
-admin.site.register(SamosaVariety)
+class SamosaReviewInline(admin.TabularInline):
+    model = SamosaReview
+    extra = 2
+
+class SamosaVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [SamosaReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('samosa_varieties',)
+
+class SamosaCertificateAdmin(admin.ModelAdmin):
+    list_display = ('samosa', 'certificate_number')
+
+admin.site.register(SamosaVariety,SamosaVarietyAdmin)
+admin.site.register(Store,StoreAdmin)
+admin.site.register(SamosaCertificate,SamosaCertificateAdmin)
+
 
--- djangoProject/bisu/models.py
+++ djangoProject/bisu/models.py
@@ -1,5 +1,6 @@
 from django.db import models # type: ignore
 from django.utils import timezone # type: ignore
+from django.contrib.auth.models import User #type: ignore
 
 # Create your models here.
 
@@ -21,3 +22,39 @@ class SamosaVariety(models.Model):
     def __str__(self):
         return self.name
     
+#One to many
+    
+class SamosaReview(models.Model):
+    samosa = models.ForeignKey(SamosaVariety,on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+    
+#Many to many
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    samosa_varieties = models.ManyToManyField(SamosaVariety,related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+#One to one
+
+class SamosaCertificate(models.Model):
+    samosa = models.OneToOneField(SamosaVariety, on_delete=models.CASCADE, related_name='certificate')    
+    certificate_number = models.CharField(max_length=100)
+    issued_added = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+    
+    def __str__(self):
+        return f'Certificate for {self.name.samosa}'
+    
+    
+    
+

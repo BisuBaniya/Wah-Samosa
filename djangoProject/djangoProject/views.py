042c4f9a3a21a7b50b2a1f85c64de01e4cdce581
BisuBaniya <bisusiddharth1806@gmail.com>
2024-07-15 21:59:15 +0530

    15 July Commit

--- djangoProject/djangoProject/views.py
+++ djangoProject/djangoProject/views.py
@@ -1,14 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse # type: ignore
+from django.shortcuts import render # type: ignore
 
 def home(request):
     # return HttpResponse("Hello, World. You are at Bisu's Home Page")
-    return render(request,'website/index.html')
+    context = {
+        'path':request.path,
+        'method':request.method,
+        'headers':dict(request.headers),
+    }
+    return render(request,'website/index.html',context)
 
 def about(request):
     return HttpResponse("Hello, World. You are at Bisu's About Page")
 
 def contact(request):
-    return HttpResponse("Hello, World. You are at Bisu's Contact Page")
+    # return HttpResponse("Hello, World. You are at Bisu's Contact Page")
+    htmlContent = "<h1><i>Hello, World. You are at Bisu's Contact Page</i></h1>"
+    return HttpResponse(htmlContent)
 
 
